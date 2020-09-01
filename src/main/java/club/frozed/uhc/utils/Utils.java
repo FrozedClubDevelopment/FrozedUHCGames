12d70ca000bb9a2a4db6a086d0aacbc92c2b6166
Elb1to <elbitomc@gmail.com>
2020-09-01 16:03:32 -0500

    Added Player Ping util

--- src/main/java/club/frozed/uhc/utils/Utils.java
+++ src/main/java/club/frozed/uhc/utils/Utils.java
@@ -14,4 +14,15 @@ public class Utils {
             players.add(player);
         return players;
     }
+
+    public static int getPing(Player p) {
+        try {
+            String version = Bukkit.getServer().getClass().getPackage().getName().substring(23);
+            Class<?> craftPlayer = Class.forName("org.bukkit.craftbukkit." + version + ".entity.CraftPlayer");
+            Object handle = craftPlayer.getMethod("getHandle", new Class[0]).invoke(p, new Object[0]);
+            return (Integer) handle.getClass().getDeclaredField("ping").get(handle);
+        } catch (Exception e) {
+            return -1;
+        }
+    }
 }
