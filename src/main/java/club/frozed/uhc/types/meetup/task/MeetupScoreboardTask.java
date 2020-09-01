6a0f15b824bdaed3856b000ea8b0d77999860659
Elb1to <elbitomc@gmail.com>
2020-09-01 15:23:38 -0500

    Added brackets

--- src/main/java/club/frozed/uhc/types/meetup/task/MeetupScoreboardTask.java
+++ src/main/java/club/frozed/uhc/types/meetup/task/MeetupScoreboardTask.java
@@ -3,14 +3,15 @@ package club.frozed.uhc.types.meetup.task;
 import club.frozed.uhc.types.meetup.provider.MeetupScoreboard;
 import club.frozed.uhc.utils.Utils;
 import club.frozed.uhc.utils.scoreboard.Board;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class MeetupScoreboardTask extends BukkitRunnable {
     @Override
     public void run() {
-        if (Board.getBoards().keySet().isEmpty()) return;
+        if (Board.getBoards().keySet().isEmpty()) {
+            return;
+        }
         try {
             for (Player player : Utils.getOnlinePlayers()) {
                 MeetupScoreboard meetupScoreboard = (MeetupScoreboard) Board.getBoards().get(player.getUniqueId());
