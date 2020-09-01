99d49acc91783242bd58835bfab84d2ca02f7598
Elb1to <elbitomc@gmail.com>
2020-09-01 16:32:10 -0500

    Spacing

--- src/main/java/club/frozed/uhc/types/meetup/task/StartingGameTask.java
+++ src/main/java/club/frozed/uhc/types/meetup/task/StartingGameTask.java
@@ -1,6 +1,7 @@
 package club.frozed.uhc.types.meetup.task;
 
 import club.frozed.uhc.FrozedUHCGames;
+import club.frozed.uhc.types.meetup.manager.game.MeetupGameManager;
 import club.frozed.uhc.utils.CC;
 import club.frozed.uhc.utils.Utils;
 import org.bukkit.Bukkit;
@@ -11,19 +12,20 @@ public class StartingGameTask extends BukkitRunnable {
     @Override
     public void run() {
 
-        int startTime = FrozedUHCGames.getInstance().getMeetupGameManager().getStartingTime();
+        FrozedUHCGames.getInstance().getMeetupGameManager().setState(MeetupGameManager.State.STARTING);
 
+        int startTime = FrozedUHCGames.getInstance().getMeetupGameManager().getStartingTime();
         String type = "second" + ((startTime > 1) ? "s" : "");
 
         startTime--;
 
-        if (startTime <= 0){
+        if (startTime <= 0) {
             Bukkit.broadcastMessage("ACA SE INCIAR EL JUEGO PX");
             cancel();
             return;
         }
 
-        switch (startTime){
+        switch (startTime) {
             case 30:
             case 25:
             case 20:
@@ -31,13 +33,15 @@ public class StartingGameTask extends BukkitRunnable {
             case 10:
             case 5:
             case 4:
+            case 3:
             case 2:
             case 1:
                 Bukkit.broadcastMessage(CC.translate(FrozedUHCGames.getInstance().getMeetupMessagesConfig().getConfig().getString("COUNTER.START"))
-                        .replace("<time>",String.valueOf(startTime))
-                        .replace("<type>",type));
-                if (!FrozedUHCGames.getInstance().getMeetupMainConfig().getConfig().getString("SOUNDS.START").equalsIgnoreCase("none") || FrozedUHCGames.getInstance().getMeetupMainConfig().getConfig().getString("SOUNDS.START") != null){
-                    Utils.getOnlinePlayers().forEach(player -> player.playSound(player.getLocation(), Sound.valueOf(FrozedUHCGames.getInstance().getMeetupMainConfig().getConfig().getString("SOUNDS.START")),2F,2F));
+                        .replace("<time>", String.valueOf(startTime))
+                        .replace("<type>", type));
+
+                if (!FrozedUHCGames.getInstance().getMeetupMainConfig().getConfig().getString("SOUNDS.START").equalsIgnoreCase("none") || FrozedUHCGames.getInstance().getMeetupMainConfig().getConfig().getString("SOUNDS.START") != null) {
+                    Utils.getOnlinePlayers().forEach(player -> player.playSound(player.getLocation(), Sound.valueOf(FrozedUHCGames.getInstance().getMeetupMainConfig().getConfig().getString("SOUNDS.START")), 2F, 2F));
                 }
                 break;
             default:
