112e0fdaeff7f2d84e6f93d3b76c65b7aacda6bb
Alex Avila <alex.ramon2004@gmail.com>
2020-09-04 20:50:59 -0500

    NMS

--- src/main/java/club/frozed/uhc/nms/version/v1_8_R3.java
+++ src/main/java/club/frozed/uhc/nms/version/v1_8_R3.java
@@ -7,6 +7,7 @@ import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 import java.util.HashMap;
 
