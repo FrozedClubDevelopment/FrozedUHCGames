2fbb475c18e530a04e965537b138b6f1a0958cc3
Alex Avila <alex.ramon2004@gmail.com>
2020-09-05 22:43:58 -0500

    Kits

---
+++ src/main/java/club/frozed/uhc/types/meetup/kit/KitManager.java
@@ -0,0 +1,11 @@
+package club.frozed.uhc.types.meetup.kit;
+
+/**
+ * Created by Ryzeon
+ * Project: FrozedUHCGames
+ * Date: 5/09/2020 @ 19:46
+ * Template by Elp1to
+ */
+
+public class KitManager {
+}
---
+++ src/main/java/club/frozed/uhc/types/meetup/kit/command/KitCommand.java
@@ -0,0 +1,11 @@
+package club.frozed.uhc.types.meetup.kit.command;
+
+/**
+ * Created by Ryzeon
+ * Project: FrozedUHCGames
+ * Date: 5/09/2020 @ 19:52
+ * Template by Elp1to
+ */
+
+public class KitCommand {
+}
